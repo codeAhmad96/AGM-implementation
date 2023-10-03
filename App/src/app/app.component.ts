0378627ab0aa5a551c74de4724023087c9f1dd6d
Muhammad Ahmad <93183309+codeAhmad96@users.noreply.github.com>
2023-10-03 19:31:12 +0500

    @angular/google-maps Installed and working

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'App';
+export class AppComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+  display: any; // Property to store latitude and longitude data from the map
+  center: google.maps.LatLngLiteral = {
+    // Initial center coordinates for the map
+    lat: 31.51679331043587,
+    lng: 74.35149289364826,
+  };
+  zoom = 4; // Initial zoom level for the map
+  move(event: google.maps.MapMouseEvent) {
+    // Method to handle map click event and update the display property
+    if (event.latLng != null) {
+      this.display = event.latLng.toJSON();
+    }
+  }
 }
