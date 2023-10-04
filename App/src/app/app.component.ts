6046894b7a6a68d2226b42760d6e4445b354c867
Muhammad Ahmad <93183309+codeAhmad96@users.noreply.github.com>
2023-10-04 10:35:48 +0500

    Markers added

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  @ViewChild(GoogleMap) map!: GoogleMap;
+  constructor() { }
+
   ngOnInit(): void {}
-  display: any; // Property to store latitude and longitude data from the map
-  center: google.maps.LatLngLiteral = {
-    // Initial center coordinates for the map
-    lat: 31.51679331043587,
-    lng: 74.35149289364826,
+
+  ngAfterViewInit() {
+    // For street view
+    const streetView = this.map.getStreetView();
+    streetView.setOptions({
+      position: { lat: 38.9938386, lng: -77.2515373 },
+      pov: { heading: 70, pitch: -10 },
+    });
+
+    streetView.setVisible(true);
+  }
+  // 30.238898158958982, 71.52564286425864
+  mapOptions: google.maps.MapOptions = {
+    center: { lat: 30.238898158958982, lng: 71.52564286425864 },
+    zoom: 14,
+    disableDefaultUI: true,
   };
+
+  marker = {
+    position: { lat: 30.238898158958982, lng: 71.52564286425864 },
+  };
+  display: any; // Property to store latitude and longitude data from the map
+  // center: google.maps.LatLngLiteral = {
+  //   // Initial center coordinates for the map
+  //   lat: 31.51679331043587,
+  //   lng: 74.35149289364826,
+  // };
   zoom = 4; // Initial zoom level for the map
   move(event: google.maps.MapMouseEvent) {
     // Method to handle map click event and update the display property
